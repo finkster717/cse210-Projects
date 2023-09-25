f23158186388eb04f09fb079ba99d415511cfce3
finkster717 <michael.fink1995@gmail.com>
2023-09-25 19:17:36 -0400

    learning03 complete

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,61 @@
+using System.Dynamic;
+using System.Xml.Serialization;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        // Initializing the fraction to 1
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        // Initializing denominator to 1 and numerator to wholeNumber parameter
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        // Initializing fraction to set parameters
+        _top = numerator;
+        _bottom = denominator;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        
+        return (double)_top/(double)_bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine(); // whitespace
+
+        // demonstrating first constructor and associated methods
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"The first fraction is: {fraction1.GetFractionString()}");
+        Console.WriteLine($"The first fraction's decimal value is: {fraction1.GetDecimalValue()}");
+
+        // demonstrating second constructor and associated methods
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"The second fraction is: {fraction2.GetFractionString()}");
+        Console.WriteLine($"The second fraction's decimal value is {fraction2.GetDecimalValue()}");
+
+        // demonstrating third constructor and associated methods
+        Fraction fraction3 = new Fraction(3,4);
+        Console.WriteLine($"The third fraction is: {fraction3.GetFractionString()}");
+        Console.WriteLine($"The third fraction's decimal value is: {fraction3.GetDecimalValue()}");
+
+        // demonstrating one more for good measure
+        Fraction fraction4 = new Fraction(1,3);
+        Console.WriteLine($"The fourth fraction is: {fraction4.GetFractionString()}");
+        Console.WriteLine($"The fourth fraction's decimal value is: {fraction4.GetDecimalValue()}");
+    
+        Console.WriteLine(); // whitespace
     }
 }
